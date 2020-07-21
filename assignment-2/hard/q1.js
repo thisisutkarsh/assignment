a2f07d61c60cd4a672a34f1a2478ce956f11db2d
thisisutkarsh <kr.utkarsh777@gmail.com>
2020-07-22 01:31:46 +0530

    Assignment-2 completed

--- assignment-2/hard/q1.js
+++ assignment-2/hard/q1.js
@@ -1,7 +1,28 @@
 // Create a function that takes a value as an argument and returns the type of this value.
 // You should get the real type of a value (JavaScript typeof doesn't return
-// the real object type of values and you need to fix that).
+// the real valueect type of values and you need to fix that).
 
 function realType(value) {
-
+    switch (typeof (value)) {
+        case 'object':
+            if (value instanceof Array)
+                return 'Array';
+            if (value instanceof Date)
+                return 'Date';
+            if (value instanceof RegExp)
+                return 'regexp';
+            if (value instanceof String)
+                return 'String';
+            if (value instanceof Number)
+                return 'Number';
+            else if (value === null)
+                return "null";
+            return 'object';
+            // valueect literals
+        default:
+            return typeof (value);
+    }
 }
+
+
+console.log(realType([]));
