11807d0c9402b26c8387a35b0e700f0e2b2dff09
thisisutkarsh <kr.utkarsh777@gmail.com>
2020-07-22 00:09:19 +0530

    update q3.js

--- assignment-2/easy/q3.js
+++ assignment-2/easy/q3.js
@@ -3,13 +3,11 @@
 // a = 1, e = 2, i = 3, o = 4, u = 5
 
 function replaceVowel(input) {
-    // let count = 0;
-    const regex = /a|e|i|o|u/g;
-    const output = input.replace(regex, );
-    // for (let i = 0; i < input.length; i++) {
-    //     let testStr = new RegExp(input[i], g);
-    //     let count = input.match(testStr)
-    // }
+    let output = input.replace(/a/g, 1);
+    output = output.replace(/e/g, 2);
+    output = output.replace(/i/g, 3);
+    output = output.replace(/o/g, 4);
+    output = output.replace(/u/g, 5);
     return console.log(output);
 }
 replaceVowel("karachi")
