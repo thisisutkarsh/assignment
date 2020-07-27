3db3f15bd2a71573b461c0cd6cafee6e5f772f6e
thisisutkarsh <kr.utkarsh777@gmail.com>
2020-07-27 22:45:47 +0530

    complete exercise-3

--- assignment-3/ExerciseEvents.js
+++ assignment-3/ExerciseEvents.js
@@ -27,29 +27,82 @@ for (let i = 0; i < 3; i++) {
   Add a further validation to check if the user input in the password and confirm password inputs match.  Show an error message if they do not.
 */
 
+document.getElementById("password").setAttribute("name", "password");
+document
+  .getElementById("confirmPassword")
+  .setAttribute("name", "ConfirmPassword");
+
+// Do below if you want to set attribute and then move forward
+// document
+//   .getElementById("registrationForm")
+//   .setAttribute("onSubmit", "return validatePassword(this)");
+// Function to check Whether both passwords
+//  is same or not.
+// function checkPassword(form) {
+//   password1 = form.password1.value;
+//   password2 = form.password2.value;
+
+
+//  By adding event listener
+const btn = document.querySelector(".btn").addEventListener("click", validatePassword);
+
+function validatePassword(pwd) {
+  password = document.getElementById("password").value;
+  confirmPassword = document.getElementById("confirmPassword").value
+  console.log(password);
+
+  // If password not entered
+  if (password == "") alert("Please enter Password");
+
+  // If confirm password not entered
+  else if (confirmPassword == "") alert("Please enter confirm password");
+
+  // if not same return false
+  else if (password != confirmPassword) {
+    alert("\nPassword did not match: Please try again");
+    return false;
+  }
+
+  // If same return True.
+  else {
+    alert("Password Matched: Welcome!");
+    return true;
+  }
+}
+
+
+
 /*
   Exercise 04
   -----------
 Ensure the ‘Register’ button is disabled until the user has entered valid data into all the input fields.  Once they have, the registration button should then be enabled.
 */
 
-document.querySelector(".btn").setAttribute("diabled", "disabled");
+// document.querySelector(".btn").setAttribute("disabled", "disabled");
+
+document.getElementById("registrationForm").setAttribute("onSubmit", "return checkForm(this)");
+
+
 let elmnt = document.getElementById("#registrationForm");
-console.log(elmnt);
-// const checkform = () => {
-//   let elmnt = document.forms["form"];
-//   console.log(elmnt);
-//   var cansubmit = true;
+// console.log(elmnt);
+// document.querySelectorAll(".form-control").forEach((element) => {
+//   element.setAttribute("onclick", "checkForm()");
+// });
 
-//   for (var i = 0; i < f.length; i++) {
-//     if (f[i].value.length == 0) cansubmit = false;
-//   }
+const checkform = () => {
+  let elmnt = document.forms["form"];
+  console.log(elmnt);
+  var cansubmit = true;
 
-//   if (cansubmit) {
-//     document.getElementById("submitbutton").disabled = false;
-//   }
-// };
-// const register = document.querySelector(".button");
+  for (var i = 0; i < f.length; i++) {
+    if (f[i].value.length == 0) cansubmit = false;
+  }
+
+  if (cansubmit) {
+    document.getElementById("submitbutton").disabled = false;
+  }
+};
+const register = document.querySelector(".button");
 
 /*
   Exercise 05
@@ -57,8 +110,7 @@ console.log(elmnt);
 When the user clicks the ‘Register’ button, a message should be displayed informing them of a successful user registration.
 */
 
-const button = document.querySelector(".btn");
-console.log(button);
-button.addEventListener("click", () => {
-  alert("Registration succesful");
-});
+// const button = document.querySelector(".btn");
+// button.addEventListener("click", () => {
+//   alert("Registration succesful");
+// }, true);
